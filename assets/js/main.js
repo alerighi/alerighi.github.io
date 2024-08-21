e50247dea7063050f704c25e84ec641b4848aaae
alerighi <alessandro.righi@alerighi.it>
2024-08-21 21:43:50 +0200

    add basic analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,30 @@ document.addEventListener('keydown', e => {
         KEY_MAP.get(e.key).click()
     }
 })
+
+window.onload = function() {
+    // Nothing to see here. This is fully GDPR compliant, trust me.
+    // I will not use this data for anything, just to know if somebody
+    // reads what I write :). It will not be transmitted to third parties
+    // or anything like it, and it's completely anonymous.
+    // Feel free to block JavaScript or disable the local storage, if you don't want.
+
+    let id = null
+    try {
+        id = localStorage.getItem("analytics_id")
+        if (!id) {
+            id = btoa(Math.random())
+            localStorage.setItem("analytics_id", id)
+        }
+    } catch {}
+
+    fetch("https://analytics.alerighi.it/record.php", {
+        method: "post",
+        body: JSON.stringify({
+            location: document.location,
+            referrer: document.referrer,
+            date: Date(),
+            id,
+        }),
+    })
+}
